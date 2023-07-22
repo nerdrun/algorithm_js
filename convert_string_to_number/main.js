c1688d8c16283537b38818194b72ad03774d419a
kai <nerdrun.kai@gmail.com>
2023-07-22 12:32:51 +0200

    conver string to number

--- convert_string_to_number/main.js
+++ convert_string_to_number/main.js
@@ -2,7 +2,7 @@ const stringToNumber = function(str) {
   /// Unary Plus (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Unary_plus)
   /// https://262.ecma-international.org/5.1/#sec-11.4.6
   /// Type conversion http://jibbering.com/faq/notes/type-conversion/
-  /// return +str;  /// clever way
+  /// return +str;  /// be aware of that converts also boolean into number, but still clever way.
   return parseInt(str);
 }
 
