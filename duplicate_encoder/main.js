0837e1fb9745234bcd4b2d8d9242926ebfe1c13b
kai <nerdrun.kai@gmail.com>
2023-08-21 13:28:34 +0200

    duplicate encoder third day

--- duplicate_encoder/main.js
+++ duplicate_encoder/main.js
@@ -1,135 +1,18 @@
 function duplicateEncode(word){
-  const array = [...word];
+  const array = [...word].map(w => w.toLowerCase());
 
+  const duplications = [];
 
   for(let i = 0; i < array.length; i++) {
-    let count = 0;
     let syllable = array[i];
-
-    if(array[i] !== '(' && array[i] !== ')') {
-      if(syllable === array[i]) count++;
-      if(count > 1) {
-        for(let j = i; j < array.length; j++) {
-          if(syllable === array[j]) array[j] = ')';
-        }
-      }
-      if(count <= 1) {
-        for(let j = 0; j < array.length; j++) {
-          if(syllable === array[j]) array[j] = '(';
-        }
-      }
+    for(let j = i + 1; j < array.length - 1; j++) {
+      if(syllable === array[j]) duplications.push(j);
     }
   }
-
-  return array.join('');
-
-  let count = 0;
-  let syllable = array[0];
-
-  if(array[0] !== '(' && array[0] !== ')') {
-    if(syllable === array[0]) count++;
-    if(count > 1) {
-      for(let i = 0; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-    if(syllable === array[1]) count++;
-    if(count > 1) {
-      for(let i = 0; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-    if(syllable === array[2]) count++;
-    if(count > 1) {
-      for(let i = 0; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-    if(syllable === array[3]) count++;
-    if(count > 1) {
-      for(let i = 0; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-
-    if(count <= 1) {
-      for(let i = 0; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = '(';
-      }
-    }
-  }
-
-  count = 0;
-  syllable = array[1];
-  if(array[1] !== '(' && array[1] !== ')') {
-    if(syllable === array[1]) count++;
-    if(count > 1) {
-      for(let i = 1; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-    if(syllable === array[2]) count++;
-    if(count > 1) {
-      for(let i = 1; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-    if(syllable === array[3]) count++;
-    if(count > 1) {
-      for(let i = 1; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-    if(syllable === array[4]) count++;
-    if(count > 1) {
-      for(let i = 1; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-
-    if(count <= 1) {
-      for(let i = 1; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = '(';
-      }
-    }
-  }
-
-  count = 0;
-  syllable = array[2];
-  if(array[2] !== '(' && array[2] !== ')') {
-    if(syllable === array[2]) count++;
-    if(count > 1) {
-      for(let i = 2; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-    if(syllable === array[3]) count++;
-    if(count > 1) {
-      for(let i = 2; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-    if(syllable === array[4]) count++;
-    if(count > 1) {
-      for(let i = 2; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-    if(syllable === array[5]) count++;
-    if(count > 2) {
-      for(let i = 2; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = ')';
-      }
-    }
-
-    if(count <= 1) {
-      for(let i = 2; i < array.length; i++) {
-        if(syllable === array[i]) array[i] = '(';
-      }
-    }
-  }
-
-  return array.join('');
+  // for(let j = 0; j < array.length; j++) {
+  //   if(syllable === array[j]) array[j] = count > 1 ? ')' : '(';
+  // }
+  return duplications;
 }
 
 module.exports = duplicateEncode;
--- duplicate_encoder/main.test.js
+++ duplicate_encoder/main.test.js
@@ -3,25 +3,51 @@ const duplicateEncode = require('./main');
 test('a returns (', () => {
   expect(duplicateEncode('a')).toBe('(');
 });
-
 test('ab returns ((', () => {
   expect(duplicateEncode('ab')).toBe('((');
 });
-
 test('abc returns (((', () => {
   expect(duplicateEncode('abc')).toBe('(((');
 });
-
 test('aa returns ))', () => {
   expect(duplicateEncode('aa')).toBe('))');
 });
-
 test('aac returns ))(', () => {
   expect(duplicateEncode('aac')).toBe('))(');
 });
-
 test('acaa returns )())', () => {
   expect(duplicateEncode('acaa')).toBe(')())');
 });
-
-
+test('a a a returns )))))', () => {
+  expect(duplicateEncode('a a a')).toBe(')))))');
+});
+test('(((abc returns )))(((', () => {
+  expect(duplicateEncode('(((abc')).toBe(')))(((');
+});
+test(' ( (  returns )))))', () => {
+  expect(duplicateEncode(' ( ( ')).toBe(')))))');
+});
+test(')a)b  returns )()(', () => {
+  expect(duplicateEncode(')a)b')).toBe(')()(');
+});
+test('din returns (((', () => {
+  expect(duplicateEncode('din')).toBe('(((');
+});
+test('recede returns ()()()', () => {
+  expect(duplicateEncode('recede')).toBe('()()()');
+});
+test('Success returns )())())', () => {
+  expect(duplicateEncode('Success')).toBe(')())())');
+});
+test('(( @ returns ))((', () => {
+  expect(duplicateEncode('(( @')).toBe('))((');
+});
+test('(() returns ))(', () => {
+  expect(duplicateEncode('(()')).toBe('))(');
+});
+// test(' ( ( ) returns )))))(', () => {
+//   expect(duplicateEncode(' ( ( )')).toBe(')))))(');
+// });
+// test('GaGGGG@y(GcIxuGP returns )())))((()(((()(', () => {
+//   expect(duplicateEncode('GaGGGG@y(GcIxuGP')).toBe(')())))((()(((()(');
+// });
