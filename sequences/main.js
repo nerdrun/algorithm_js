99f4b8d8ee28473fe5b5079fa40fb98a0e195964
kai <nerdrun.kai@gmail.com>
2023-09-04 10:36:49 +0200

    third day

--- sequences/main.js
+++ sequences/main.js
@@ -3,41 +3,24 @@ function findCombos(array) {
 
   const result = [];
   for(let i = 0; i < array.length; i++) { 
-    const permutated = [];
-    const curr = array.slice();
-    const next = curr.splice(i, 1);
-    permutated.concat(next);
-    console.log('next : ', next);
-    console.log(permutated);
-    /// [1] [2] [3]
-    // for(let j = 0; j < curr.length; j++) {
-    //   const subCurr = curr.slice();
-    //   permutated.concat(subCurr.splice(j, 1))
-    // }
-    result.push(permutated);
+    let curr = array.slice();
+    let next = curr.splice(i, 1);
+    let m = [].concat(next);
+    for(let j = 0; j < curr.length; j++) {
+      let jCurr = curr.slice(); 
+      let next = jCurr.splice(j, 1); 
+      m = m.concat(next);
+      for(let k = 0; k < jCurr.length; k++) {
+        let kCurr = jCurr.slice();
+        let next = kCurr.splice(k, 1);
+        m = m.concat(next);
+        result.push(m);
+      }
+    }
   }
+  console.log('result : ', result);
 
-  return result;
-
-  // const flat = array.flat(Infinity).sort((a, b) => a - b)
-  // if(flat.length <= 1) return 1;
-
-  // let result = [];
-  // const permute = (arr, m = []) => {
-  //   if (arr.length === 0) {
-  //     result.push(m)
-  //   } 
-  //   else {
-  //     for (let i = 0; i < arr.length; i++) {
-  //       let curr = arr.slice();
-  //       if(i + 1 < arr.length && curr[i] === curr[i + 1]) continue;
-  //       let next = curr.splice(i, 1);
-  //       permute(curr.slice(), m.concat(next));
-  //     }
-  //   }
-  // }
-  // permute(flat);
-  // return result.length;
+  return result.length;
 }
 
 module.exports = findCombos;
