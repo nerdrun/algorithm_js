91a2f77f895a7e1990cf3fbe48f3d6dedcfc67eb
kai <nerdrun.kai@gmail.com>
2023-09-04 12:28:42 +0200

    sequences: done

--- sequences/main.js
+++ sequences/main.js
@@ -1,25 +1,25 @@
-function findCombos(array) {
-  if(array.length <= 1) return 1;
+const deepFlatten = arr => Array.isArray(arr) ? arr.flatMap(deepFlatten) : arr;
+const factorial = n => n > 1 ? n * factorial(n - 1) : 1;
 
+function findCombos(array) {
+  const arr = deepFlatten(array), cnt = arr.reduce((res, k) => (res[k] = res[k] + 1 || 1, res), {});
+  return factorial(arr.length) / Object.values(cnt).reduce((a, b) => a * factorial(b), 1);
+}
+
+/// This solution has been failed due to timeout (12,000ms) for big array
+function findCombos(array) {
+  const flat = array.flat(Infinity);
+  if(flat.length <= 1) return 1;
   const result = [];
-  for(let i = 0; i < array.length; i++) { 
-    let curr = array.slice();
-    let next = curr.splice(i, 1);
-    let m = [].concat(next);
-    for(let j = 0; j < curr.length; j++) {
-      let jCurr = curr.slice(); 
-      let next = jCurr.splice(j, 1); 
-      m = m.concat(next);
-      for(let k = 0; k < jCurr.length; k++) {
-        let kCurr = jCurr.slice();
-        let next = kCurr.splice(k, 1);
-        m = m.concat(next);
-        result.push(m);
-      }
+  const premutate = (array, m = []) => {
+    if(!array.length && result.findIndex(val => val.toString() === m.toString()) === -1) result.push(m);
+    for(let i = 0; i < array.length; i++) {
+      const curr = array.slice();
+      const next = curr.splice(i, 1);
+      premutate(curr, m.concat(next));
     }
   }
-  console.log('result : ', result);
-
+  premutate(flat);
   return result.length;
 }
 
--- sequences/main.test.js
+++ sequences/main.test.js
@@ -7,9 +7,6 @@ describe('extract all possible orders', () => {
   test('[1,2,3,4] returns 24', () => {
     expect(findCombos([1,2,3,4])).toStrictEqual(24);
   });
-  // test('[1,2,3,4,5] returns 120', () => {
-  //   expect(findCombos([1,2,3,4,5])).toStrictEqual(120);
-  // });
   test('[1,2,2] returns 3', () => {
     expect(findCombos([1,2,2])).toStrictEqual(3);
   });
