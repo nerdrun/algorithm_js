39dd8e32a518bc67d1ce0e1280d3738d35b1286b
kai <nerdrun.kai@gmail.com>
2023-09-10 09:23:24 +0200

    two sum: done but required refactor

--- two_sum/main.js
+++ two_sum/main.js
@@ -1,14 +1,12 @@
 function twoSum(numbers, target) {
-  let result;
   for(let i = 0; i < numbers.length - 1; i++) {
     let sub = target - numbers[i];
     for(let j = i + 1; j < numbers.length; j++) {
       if(sub === numbers[j]) {
-        result = [numbers[i], numbers[j]];
+        return [i, j];
       }
     }
   }
-  return result;
 }
 
 module.exports = twoSum;
