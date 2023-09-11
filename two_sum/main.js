3627b22c56ca5a5997873941b9e3149c3b1d2d8d
kai <nerdrun.kai@gmail.com>
2023-09-11 10:58:10 +0200

    two sum: refactoring

--- two_sum/main.js
+++ two_sum/main.js
@@ -1,4 +1,13 @@
 function twoSum(numbers, target) {
+  /// This for loop has a time complexity of O(n).
+  const seen = new Map();
+  for(let i = 0; i < numbers.length; i++) {
+    let value = numbers[i], sub = target - numbers[i];
+    if(seen.has(sub)) return [seen.get(sub), i];
+    seen.set(value, i);
+  }
+
+  /// This for loop has a time complexity of O(n^2).
   for(let i = 0; i < numbers.length - 1; i++) {
     let sub = target - numbers[i];
     for(let j = i + 1; j < numbers.length; j++) {
