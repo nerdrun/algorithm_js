e5304dc70e2e1810d5dd5fcdb3696dc2b86bb10f
kai <nerdrun.kai@gmail.com>
2023-10-01 17:16:51 +0200

    take a number and sum: done

--- take_a_number_and_sum_its_digits_raised_to_the_consecutive_powers_and_eureka/main.js
+++ take_a_number_and_sum_its_digits_raised_to_the_consecutive_powers_and_eureka/main.js
@@ -1,41 +1,5 @@
 function sumDigPow(a, b) {
-  let result = [];
-
-  for(let i = a, j = 0; i <= b; i++, j++) {
-    let sum = 1;
-    for(let k = 0; k < (j + 1); k++) {
-      sum *= a;
-    }
-    console.log('sum : ', sum);
-    result.push(sum);
-  }
-  // if(split.length > 0) {
-  //   let test = 1;
-  //   for(let i = 0; i < 1; i++) {
-  //     test *= +split[0];
-  //   }
-  //   result += test;
-  // }
-  // if(split.length > 1) {
-  //   let test = 1;
-  //   for(let i = 0; i < 2; i++) {
-  //     test *= +split[1];
-  //   }
-  //   result += test;
-  // }
-  // if(split.length > 2) {
-  //   let test = 1;
-  //   for(let i = 0; i < 3; i++) {
-  //     test *= +split[2];
-  //   }
-  //   result += test;
-  // }
-  return result;
-  // for(let i = a; i <= b; i++) {
-  //   const split = i.toString().split('');
-  //   result.push(test);
-  // }
-  // return result;
+  return Array.from({ length: ++b - a }, (_, i) => a + i).filter((v) => v === v.toString().split('').map(Number).reduce((acc, cur, idx) => (acc += Math.pow(+cur, idx + 1), acc), 0));
 }
 
 module.exports = sumDigPow;
--- take_a_number_and_sum_its_digits_raised_to_the_consecutive_powers_and_eureka/main.test.js
+++ take_a_number_and_sum_its_digits_raised_to_the_consecutive_powers_and_eureka/main.test.js
@@ -1,15 +1,13 @@
 const sumDigPow = require('./main');
 
-/// take apart a number to 10th and 1st.
-
 describe('take a number and sum its digits raised to the consecutive power', () => {
   test('1,2 returns [1,2]', () => {
     expect(sumDigPow(1,2)).toStrictEqual([1,2]);
   });
+  test('8,10 returns [8,9]', () => {
+    expect(sumDigPow(8,10)).toStrictEqual([8,9]);
+  });
+  test('1,100 returns [1,2,3,4,5,6,7,8,9,89]', () => {
+    expect(sumDigPow(1,100)).toStrictEqual([1,2,3,4,5,6,7,8,9,89]);
+  });
 });
-
-// describe('take apart a number to 10th and ones', () => {
-//   test('[89, 90] must return [89]', () => {
-//     expect(sumDigPow([10,11])).
-//   });
-// });
