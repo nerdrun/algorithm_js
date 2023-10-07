090811a79b44edfd85b6c9b8ac300c6deb3f0a00
kai <nerdrun.kai@gmail.com>
2023-10-07 18:35:19 +0200

    count characters: done

--- count_characters_in_your_string/main.js
+++ count_characters_in_your_string/main.js
@@ -1,21 +1,5 @@
 function count(string) {
-  const test = undefined;
-  console.log(!!test + 1);
-  return string.split('').reduce((acc, cur) => (acc[cur] += !!acc[cur] + 1, acc), {});
-
-  // const result = {};
-  // const split = string.split('');
-  // if(split.length) {
-  //   for(let i = 0; i < split.length; i++) {
-  //     if(result[split[i]]) {
-  //       result[split[i]] += 1;
-  //     }
-  //     else {
-  //       result[split[i]] = 1;
-  //     }
-  //   }
-  // }
-  // return result;
+  return string.split('').reduce((acc, cur) => (acc[cur] ? acc[cur]++ : acc[cur] = 1, acc), {});
 }
 
 module.exports = count;
